d3a1dce1413fd15b8156828f11a4f368cbadbb91
Maximilien Cuony <maximilien.cuony@arcanite.ch>
2022-07-06 10:54:43 +0200

    Add option to remove duplicate and list all headers on a single line

--- options/options.js
+++ options/options.js
@@ -7,14 +7,18 @@ function saveOptions(e) {
 
     browser.storage.local.set({
         headerNumbers: document.querySelector("#header-numbers").value,
-        regexp: document.querySelector("#regexp").value
+        regexp: document.querySelector("#regexp").value,
+        removeDuplicates: document.querySelector("#remove-duplicates").checked,
+        singleLine: document.querySelector("#single-line").checked
     });
 }
 
 function restoreOptions() {
-    browser.storage.local.get(["headerNumbers", "regexp"]).then(({headerNumbers, regexp}) => {
+    browser.storage.local.get(["headerNumbers", "regexp", "removeDuplicates", "singleLine"]).then(({headerNumbers, regexp, removeDuplicates, singleLine}) => {
         document.querySelector("#header-numbers").value = headerNumbers;
         document.querySelector("#regexp").value = regexp;
+        document.querySelector("#remove-duplicates").checked = removeDuplicates;
+        document.querySelector("#single-line").checked = singleLine;
     });
 }
 
